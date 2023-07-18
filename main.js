12dde868b0bacb611d0a57160bcff3f27481a7be
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-07-19 01:28:34 +0300

    Add simple ui to chose the algorithm, bars count and delay

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let container = document.getElementById("line-container");
+let start = document.getElementById("start")
+let selectdElement = document.getElementById('heuristic');
+let refresh = document.getElementById("refresh")
+let nobars = document.getElementById('nobars');
+let delay = document.getElementById('delay');
 
 let screenWidth = window.innerWidth;
 
@@ -6,35 +11,41 @@ let columnWidth = screenWidth / 50;
 let numColumns = Math.floor(screenWidth / columnWidth);
 let randomValues = [];
 
-let sleepDuaration = 10;
+let sleepDuaration = 0;
 
+//---colors---//
+let barcolor = "#001C30"
+let sortedcolor = "#176B87"
+let endcolor = "#64CCC5"
+let curcolor = "#CCEEBC"
+
+let bars;
+let barArr;
 initBars(container, numColumns);
-let bars = document.querySelectorAll('span');
-let barArr = Array.from(bars);
 
-bubblesort(randomValues);
 
-//--------sfrequancy sort--------//
+//--------frequancy sort--------//
 async function frequancysort(arr){
     for (let i = 0; i < arr.length; i++) {
-        barArr[i].style.backgroundColor = "yellow";
+        barArr[i].style.backgroundColor = curcolor;
         await sleep(sleepDuaration)
-        barArr[i].style.backgroundColor = "black";
+        barArr[i].style.backgroundColor = barcolor;
     }
     arr.sort();
     await resizebars(barArr, arr)
     await animationEnd(barArr)
 }
+
 //--------selection sort--------//
 async function quickSort(arr, left = 0, right = arr.length - 1) {
     if (left < right) {
       const partitionIndex = await partition(arr, left, right);
 
       await quickSort(arr, left, partitionIndex - 1);
-      barArr[right].style.backgroundColor = "green";
+      barArr[right].style.backgroundColor = sortedcolor;
       await quickSort(arr, partitionIndex + 1, right);
-      await setBarColors(left, right, "green")
-      barArr[right].style.backgroundColor = "green";
+      await setBarColors(left, right, sortedcolor)
+      barArr[right].style.backgroundColor = sortedcolor;
     }
     if (left == 0 && right == arr.length - 1)
         animationEnd(barArr)
@@ -42,9 +53,9 @@ async function quickSort(arr, left = 0, right = arr.length - 1) {
   
   async function partition(arr, left, right) {
     const pivot = arr[right];
-    barArr[right].style.backgroundColor = "yellow";
-    await setBarColors(left, right, "green")
-    await setBarColors(left, right, "black")
+    barArr[right].style.backgroundColor = curcolor;
+    await setBarColors(left, right, sortedcolor)
+    await setBarColors(left, right, barcolor)
 
     let i = left - 1;
     
@@ -59,20 +70,25 @@ async function quickSort(arr, left = 0, right = arr.length - 1) {
     swap(arr, i + 1, right);
     return i + 1;
   }
+
 //--------bubble sort--------//
 async function bubblesort(arr) {
+  let flag = 1
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
         if (arr[j] > arr[j + 1]) {
-        	barArr[j].style.backgroundColor = "yellow";
-        	barArr[j + 1].style.backgroundColor = "yellow";
+        	barArr[j].style.backgroundColor = curcolor;
+        	barArr[j + 1].style.backgroundColor = curcolor;
         	swap(arr, j, j + 1);
         	await sleep(sleepDuaration);
-        	barArr[j].style.backgroundColor = "black";
-        	barArr[j + 1].style.backgroundColor = "black";
+        	barArr[j].style.backgroundColor = barcolor;
+        	barArr[j + 1].style.backgroundColor = barcolor;
+          flag = 0
         }
     }
-    barArr[arr.length - 1 - i].style.backgroundColor = "green";
+    if (flag)
+      return
+    barArr[arr.length - 1 - i].style.backgroundColor = sortedcolor;
   }
   await animationEnd(barArr);
 }
@@ -83,51 +99,52 @@ async function selectionSort(arr) {
   
     for (let i = 0; i < length - 1; i++) {
       let minIndex = i;
-      barArr[i].style.backgroundColor = "yellow";
+      barArr[i].style.backgroundColor = curcolor;
   
       for (let j = i + 1; j < length; j++) {
-        barArr[j].style.backgroundColor = "yellow";
+        barArr[j].style.backgroundColor = curcolor;
         if (arr[j] < arr[minIndex]) {
           minIndex = j;
         }
         await sleep(sleepDuaration);
-        barArr[j].style.backgroundColor = "black";
+        barArr[j].style.backgroundColor = barcolor;
       }
   
       if (minIndex !== i) {
         swap(arr, i, minIndex);
       }
-      barArr[i].style.backgroundColor = "green";
+      barArr[i].style.backgroundColor = sortedcolor;
     }
-    barArr[length - 1].style.backgroundColor = "green";
+    barArr[length - 1].style.backgroundColor = sortedcolor;
     await animationEnd(barArr);
   }
 
 //--------insertion sort--------//
-async function insertionSort(array) {
-    for (let i = 1; i < array.length; i++) {
+async function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
       let j = i;
-      insertionSetBarColors(0, i, "green");
+      insertionSetBarColors(0, i, sortedcolor);
   
-      while (j > 0 && array[j] < array[j - 1]) {
+      while (j > 0 && arr[j] < arr[j - 1]) {
         sleep(sleepDuaration)
-        insertionSetBarColors(0, j, "yellow");
-        swap(array, j, j - 1);
-        barArr[j].style.backgroundColor = "green";
+        insertionSetBarColors(0, j, curcolor);
+        swap(arr, j, j - 1);
+        barArr[j].style.backgroundColor = sortedcolor;
         j--;
-        insertionSetBarColors(0, j, "green");
+        insertionSetBarColors(0, j, sortedcolor);
         await sleep(sleepDuaration);
       }
-      insertionSetBarColors(0, i, "green");
+      insertionSetBarColors(0, i, sortedcolor);
       
     }
      animationEnd(barArr);
   }
 
-
 //--------helper functions--------//
 
 function initBars(container, numColumns){
+    randomValues = []
+    container.innerHTML = '';
     for (let i = 0; i < numColumns; i++) {
       randomValues.push(Math.random());
     }
@@ -140,11 +157,13 @@ function initBars(container, numColumns){
       bar.style.left = (i * columnWidth) + "px";
       bar.style.bottom = "0";
       bar.style.width = columnWidth - 1 + "px";
-      bar.style.height = (randomValues[i] * 100) + "%";
-      bar.style.backgroundColor = "black";
+      bar.style.height = (randomValues[i] * 90) + "%";
+      bar.style.backgroundColor = barcolor;
     
       container.appendChild(bar);
     }
+    bars = document.querySelectorAll('span');
+    barArr = Array.from(bars);
 }
 
 async function setBarColors(start, end, color) {
@@ -161,7 +180,7 @@ function insertionSetBarColors(start, end, color) {
 
 async function animationEnd(barArr){
   for (let k = 0; k < barArr.length; k++) {
-      barArr[k].style.backgroundColor = "red";
+      barArr[k].style.backgroundColor = endcolor;
       await sleep(sleepDuaration)
   }
 }
@@ -182,12 +201,52 @@ function swap(arr, a, b) {
 
 async function resizebars(barArr, arr){
     for (let k = 0; k < barArr.length; k++) {
-        barArr[k].style.height = (arr[k] * 100) + "%";
-        barArr[k].style.backgroundColor = "green";
+        barArr[k].style.height = (arr[k] * 90) + "%";
+        barArr[k].style.backgroundColor = sortedcolor;
         await sleep(sleepDuaration)
     }
 }
+//---------buttons action-------------//
+refresh.addEventListener('click', function(){
+  location.reload();
+})
 
+start.addEventListener('click', async function(){
+  if (nobars.value)
+  {
+    numColumns = nobars.value? parseInt(nobars.value): 50
+    columnWidth = Math.floor(screenWidth / numColumns);
+    initBars(container, numColumns)
+  }
+  sleepDuaration = delay.value ? parseInt(delay.value):0
+  switch (selectdElement.value){
+  case "0":
+    start.disabled  = true
+    await selectionSort(randomValues)
+    start.disabled  = false
+    break;
+  case "1":
+    start.disabled  = true
+    bubblesort(randomValues)
+    start.disabled  = false
+    break;
+  case "2":
+    start.disabled  = true
+    insertionSort(randomValues)
+    start.disabled  = false
+    break;
+  case "3":
+    start.disabled  = true
+    quickSort(randomValues)
+    start.disabled  = false
+    break;
+  case "4":
+    start.disabled  = true
+    frequancysort(randomValues, barArr)
+    start.disabled  = false
+    break;
+  }
+})
 //-----------old ideas-----------//
 // function swapDivs(span1, span2) {
 //     let parent1 = span1.parentNode;
